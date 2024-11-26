281697930f4d7b3281cb568372d0d7deb02ff2b2
First-project <mehedihasan0937580@gmail.com>
2024-11-26 23:45:10 +0600

    Train section done

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "./Hero";
 import Partners from "./Partners";
 import Offers from "./Offers";
+import Train from "./Train";
 
 const HomePage = () => {
   return (
@@ -9,6 +10,7 @@ const HomePage = () => {
       <Hero />
       <Partners />
       <Offers />
+      <Train />
     </div>
   );
 };
---
+++ src/components/Home/Train.jsx
@@ -0,0 +1,54 @@
+import { FaRegCheckCircle } from "react-icons/fa";
+
+const Train = () => {
+  const list = [
+    {
+      icon: <FaRegCheckCircle />,
+      title: "Quick to ramp",
+    },
+    {
+      icon: <FaRegCheckCircle />,
+      title: "Easy to optimize",
+    },
+    {
+      icon: <FaRegCheckCircle />,
+      title: "Quick to scale up",
+    },
+    {
+      icon: <FaRegCheckCircle />,
+      title: "Works with all your existing tools",
+    },
+  ];
+  return (
+    <section className="bg-[url(/train-bg.png)] bg-no-repeat bg-cover sm:mx-12 mx-8 sm:py-28 py-8 ">
+      <div className="flex justify-center items-center">
+        <div className="lg:ps-60 md:ps-40 sm:ps-20 ps-6">
+          <h3 className="text-white font-Montserrat font-semibold sm:text-[40px] text-[28px] sm:leading-[48px] leading-8 -tracking-[1%]">
+            Train your aiDR on your...
+          </h3>
+          <h4 className="text-primary font-Montserrat italic font-[500] sm:text-[40px] text-[28px] sm:leading-[48px] leading-[34px] -tracking-[1%]">
+            prefered email st|
+          </h4>
+          <div className="sm:pt-[30px] pt-5 sm:pb-16 pb-10">
+            <p className="text-secondary  font-workSans font-[300] sm:text-2xl text-[18px] sm:leading-[31px] leading-[23px] -tracking-[1%] max-w-md">
+              You&apos;re in control. Train your aiDR on elements of your
+              Marketing strategy.
+            </p>
+          </div>
+          <div className="grid sm:grid-cols-2 grid-cols-1 gap-3">
+            {list.map((item, index) => (
+              <div key={index} className="flex items-center gap-1">
+                <span className="text-cyan">{item.icon}</span>
+                <p className="text-cyan font-Montserrat font-[500] sm:text-[20px] text-base sm:leading-[26px] leading-5 -tracking-[1%]">
+                  {item.title}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Train;

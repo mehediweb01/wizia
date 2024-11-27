19a6dea167f068da77e112afacaec45461081f27
First-project <mehedihasan0937580@gmail.com>
2024-11-27 23:26:27 +0600

    onload animation added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Buttons from "./components/Buttons";
-import { MdOutlineInsertEmoticon } from "react-icons/md";
 import NavbarArea from "./components/shared/Navbar";
 import HomePage from "./components/Home/HomePage";
 import PageNotFount from "./components/PageNotFount";
+import { motion } from "framer-motion";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <NavbarArea />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="*" element={<PageNotFount />} />
-        </Routes>
-      </BrowserRouter>
+      <motion.div
+        initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1.5 }}
+      >
+        <BrowserRouter>
+          <NavbarArea />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="*" element={<PageNotFount />} />
+          </Routes>
+        </BrowserRouter>
+      </motion.div>
     </>
   );
 }
